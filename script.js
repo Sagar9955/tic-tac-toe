a4bf9fd4dd2103eb4b28479cd2ed3e5b6afdb6a8
sagariaa <sagarksingh03@gmail.com>
2024-01-16 12:47:00 +0530

    completed

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let boxes = document.querySelectorAll(".box");
 let turn0=true;//for selecting player 1
+let count=0;
+let msg=document.querySelector("#msg");
+let msgcontainer=document.querySelector(".msg-container");
+let newbutton=document.querySelector("#newgm")
 boxes.forEach((box) => {
     box.addEventListener("click", () => {
         console.log("clicked");
@@ -11,7 +15,11 @@ boxes.forEach((box) => {
             turn0=true;
         }
         box.disabled=true;
-        checkwinner();
+        count++;
+        let isWinner=checkwinner();
+        if(count===9 && !isWinner){
+            gamedraw();
+        }
     });
     
 });
@@ -27,8 +35,51 @@ let winPattern=[
     [2,5,8]
 ]
 
+let showWinner =(winner)=>{
+    msg.innerText=`Congratulations the winner is ${winner}`;
+    msgcontainer.classList.remove("hide");
+    disable();
+};
+
+const gamedraw=()=>{
+    msg.innerText=`draw`;
+    msgcontainer.classList.remove("hide");
+    disable();
+}
+
 let checkwinner=()=>{
     for (let pattern of winPattern){
-        console.log(boxes[pattern[0]])
+        let pos1=boxes[pattern[0]].innerText;
+        let pos2=boxes[pattern[1]].innerText;
+        let pos3=boxes[pattern[2]].innerText;
+        if(pos1!='' && pos2!='' && pos3!=''){
+            if(pos1==pos2 && pos2==pos3 && pos1==pos3){
+                showWinner(pos1);
+            }
+        
+        
     }
+}
 };
+
+let disable=()=>{
+    for(let box of boxes){
+        box.disabled=true;
+    }
+}
+let enable=()=>{
+    turn0=true;
+    for(let box of boxes){
+        box.disabled=false;
+        box.innerText="";
+        msgcontainer.classList.add("hide");
+    }
+
+}
+
+let newgame=()=>{
+    turn0=true;
+    enable();
+}
+
+newbutton.addEventListener("click",newgame);
